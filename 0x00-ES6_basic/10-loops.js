57bd8f6125ee8a0c36b63ba1d93a64dad2839a88
Nihal <nihaleloudiki16@gmail.com>
2024-09-06 23:17:00 +0000

    javascript es6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
+    arr.push(appendString + value);
+  }
+
+  return arr;
+}
