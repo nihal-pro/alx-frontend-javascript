83cb4da44d20ae624f6b1017384666560c98f757
Nihal <nihaleloudiki16@gmail.com>
2024-09-07 20:09:12 +0000

    javascript es6

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
